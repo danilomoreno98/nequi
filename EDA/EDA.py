71e9c20f9a56874d7e31fd77e77a3775acd62320
danilomoreno98 <94095632+danilomoreno98@users.noreply.github.com>
2023-06-19 15:25:18 -0500

    Change: New image

--- EDA/EDA.py
+++ EDA/EDA.py
@@ -13,6 +13,7 @@ Methods
 """
 # Step 1: Read dataset
 def load_data(dir_file, *sheets_name):
+    print("Leyendo Dataset...")
     dfs = []
     for sheet in sheets_name:
         df = pd.read_excel(dir_file, sheet_name=sheet)
@@ -26,13 +27,14 @@ def load_data(dir_file, *sheets_name):
 def quantity_type_variables(df):
     column_names = df.columns.tolist()
     column_types = df.dtypes.tolist()
-    print("***** Exploración cantidad de variables y tipo *****")
+    print("\n***** 2. Exploración cantidad de variables y tipo *****")
     for column_name, column_type in zip(column_names, column_types):
             print(f"Columna: {column_name} -> Tipo: {column_type}")
     return column_names, column_types
 
 # Step 3: Null rows
 def null_values(df):
+    print("\n***** 3. Exploración datos nulos *****\n",df.isnull().sum())
     return df.isnull().sum()
 
 # Step 4: Unique Values "Invoices" & Customer ID "Invoice" "Customer ID"
@@ -41,12 +43,14 @@ def unique_values(df, column_name):
 
 # Step 5: Explore consistency in data
 def consistency_numeric_data(df):
+    print("\n***** 5. Exploración consistencia datos numericos *****")
     df_numeric = df.select_dtypes(include=['int64', 'float64'])
     print(df_numeric.describe().T)
 
 
 # Metodo -- MAIN ---
 def main():
+    print("++++++++++++++++++++++ Inicia Main +++++++++++++++++++++++")
     # Step 1: Read dataset
     df_final = load_data(dir_file, *sheet_names)
     
@@ -54,11 +58,11 @@ def main():
     quantity_type_variables(df_final)
     
     # Step 3: Null rows
-    print("\n ***** Exploración datos nulos *****\n", null_values(df_final))
+    null_values(df_final)
 
     # Step 4: Unique Values "Invoices" & Customer ID
-    print("\n ***** Exploración valores unicos, columna 'Invoice': {} *****".format(unique_values(df_final,"Invoice")))
-    print("\n ***** Exploración valores unicos, columna 'Customer ID': {} *****".format(unique_values(df_final,"Customer ID")))
+    print("\n ***** 4. Exploración valores unicos, columna 'Invoice': {} *****".format(unique_values(df_final,"Invoice")))
+    print("***** 4. Exploración valores unicos, columna 'Customer ID': {} *****".format(unique_values(df_final,"Customer ID")))
 
     # Step 5: consistency_numeric_data
     consistency_numeric_data(df_final)
